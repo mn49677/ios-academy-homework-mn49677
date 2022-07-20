ec80cb8cd9bb09bb92bae3e27d4636804df51c56
Max <max.novak5@gmail.com>
2022-07-20 22:01:48 +0200

    homework-02-03 Add HomeViewController and User class for networking

---
+++ TV Shows/TV Shows/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  TV Shows
+//
+//  Created by mn on 20.07.2022..
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
