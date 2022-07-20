2c528b82827f4736784ff6c279292cd02dcc316b
Max <max.novak5@gmail.com>
2022-07-20 22:01:24 +0200

    homework-02-03 Add constants to separate file

---
+++ TV Shows/TV Shows/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  TV Shows
+//
+//  Created by mn on 20.07.2022..
+//
+
+import Foundation
+
+enum Constants {
+    enum ViewControllers {
+        static var LoginViewController = "LoginViewController"
+        static var HomeViewController = "HomeViewController"
+    }
+    
+    enum Storyboards {
+        static var LoginStoryboard = "Login"
+        static var SplashStoryboard = "SplashScreen"
+    }
+}
