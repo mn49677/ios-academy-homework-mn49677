5e53ebb165a9042344183b505f83e9cf02a00572
Max <max.novak5@gmail.com>
2022-07-20 22:01:03 +0200

    homework-02-03 Add splash screen

---
+++ TV Shows/TV Shows/Login/Elements/UnderlinedTextField.swift
@@ -0,0 +1,42 @@
+//
+//  UnderlinedTextField.swift
+//  TV Shows
+//
+//  Created by mn on 19.07.2022..
+//
+
+import UIKit
+
+final class UnderlinedTextField: UITextField {
+
+    let underlineLayer = CALayer()
+
+    /// Size the underline layer and position it as a one point line under the text field.
+    func setupUnderlineLayer() {
+        var frame = self.bounds
+        frame.origin.y = frame.size.height - 1
+        frame.size.height = 1
+
+        underlineLayer.frame = frame
+        underlineLayer.backgroundColor = UIColor.white.cgColor
+    }
+
+    // In `init?(coder:)` Add our underlineLayer as a sublayer of the view's main layer
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.layer.addSublayer(underlineLayer)
+    }
+
+    // in `init(frame:)` Add our underlineLayer as a sublayer of the view's main layer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.addSublayer(underlineLayer)
+    }
+
+    // Any time we are asked to update our subviews,
+    // adjust the size and placement of the underline layer too
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupUnderlineLayer()
+    }
+}
